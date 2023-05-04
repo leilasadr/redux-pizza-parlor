2aac8d3ffd5055862d30bb4397debb7dca793f52
Leila Sadr <leilasadr54@gmail.com>
2023-05-04 12:20:53 -0700

    We added the reducer for the pizzas array

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,24 +2,37 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 function App() {
 
   useEffect(() => {
     fetchPizzas();
-  }, [])
+  }, []);
+
+  const dispatch = useDispatch();
 
   const fetchPizzas = () => {
     axios({
       method: 'GET',
       url: '/api/pizza'
     }).then((response) => {
+      
       const pizzaArray = response.data;
+      dispatch({
+        type: 'SET_PIZZAS',
+        payload: pizzaArray
+      });
+
     }).catch((error) => {
       console.log('GET error:', error);
     });
   };
-  
+
+  const pizzas = useSelector((store) => store.pizzas);
+  console.log('Pizza from Store:', pizzas);
+
   return (
     <div className='App'>
       <header className='App-header'>
--- src/index.js
+++ src/index.js
@@ -3,9 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 
+// Redux
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+// Reducers
+const pizzas = (state = [], action) => {
+    if(action.type === 'SET_PIZZAS') {
+        const pizzaArray = action.payload;
+        return pizzaArray;
+    }
+    return state;
+
+};
+
+// Store
+const theStore = createStore(
+    combineReducers({
+        pizzas
+    }),
+    applyMiddleware(
+        logger
+    )
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={theStore}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
